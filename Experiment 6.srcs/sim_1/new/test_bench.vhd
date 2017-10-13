045ba3d56d754252444ab5419bf00087a6565f29
rsolorza <rsolorza@calpoly.edu>
2017-10-13 12:09:49 -0700

    Initial VHDL creation

---
+++ Experiment 6.srcs/sim_1/new/test_bench.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/13/2017 12:04:46 PM
+-- Design Name: 
+-- Module Name: test_bench - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity test_bench is
+--  Port ( );
+end test_bench;
+
+architecture Behavioral of test_bench is
+
+begin
+
+
+end Behavioral;
---
+++ Experiment 6.srcs/sources_1/new/ALU.vhd
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/13/2017 12:02:15 PM
+-- Design Name: 
+-- Module Name: ALU - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ALU is
+    Port ( A : in STD_LOGIC_VECTOR (7 downto 0);
+           B : in STD_LOGIC_VECTOR (7 downto 0);
+           SEL : in STD_LOGIC_VECTOR (3 downto 0);
+           Cin : in STD_LOGIC;
+           RESULT : out STD_LOGIC_VECTOR (7 downto 0);
+           C : out STD_LOGIC;
+           Z : out STD_LOGIC);
+end ALU;
+
+architecture Behavioral of ALU is
+
+begin
+
+
+end Behavioral;
